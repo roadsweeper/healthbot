1c975061a94c2a28fe89b8001953842d3f6496fd
roadsweeper <88540746+roadsweeper@users.noreply.github.com>
2021-08-17 01:11:37 +0800

    works daily
    - updated token.txt
    - works without schedule package

--- py4e(c2p1)holup.py
+++ py4e(c2p1)holup.py
@@ -3,25 +3,72 @@ import time
 import datetime
 import discord
 from discord.ext import commands
-client = commands.Bot(command_prefix=("!"),
+import asyncio
+import os
+
+client = discord.Client()
+
+
+
+'''client = commands.Bot(command_prefix=("!"),
                       case_insensitive=True,
-                      help_command=None)
+                      help_command=None)'''
+def read_token():
+    with open("token.txt", "r") as f:
+        lines = f.readlines()
+        return lines[0].strip()
+token = read_token()
+
+
 @client.event
 async def on_ready():
     print("BOT IS READY")
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Playing your mother"))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="your mother"))
+
+@client.event
+async def on_message(message):
+    if message.content.find("!hello") != -1:
+        await message.channel.send("Hi")
+        myid = '<@&876859196006600774>'
+        await message.channel.send("wake up and current time is: " + str(datetime.datetime.now()))
+        await message.channel.send(' : %s is the best ' % myid)
+    if message.content.find("!time") != -1:
+        await message.channel.send(datetime.datetime.now())
+    if message.content.find("!start") != -1:
+        await message.channel.send(datetime.datetime.now())
+        alarmHour = datetime.datetime.now().hour
+        alarmMinute = datetime.datetime.now().hour
+        stopHour = datetime.datetime.now().hour
+        stopMinute = datetime.datetime.now().minute + 3
+        myid = '<@&876859196006600774>'
+        while True:
+            # time period condition
+            if datetime.datetime.now().hour < 2 or datetime.datetime.now().hour > 8:
+                '''if stopHour == datetime.datetime.now().hour and stopMinute == datetime.datetime.now().minute:
+                    print('End' + str(datetime.datetime.now()))
+                    await message.channel.send('End' + str(datetime.datetime.now()))'''
+                if alarmMinute == datetime.datetime.now().hour:
+                    print("Water Parade and Move About Time and current time is: " + str(datetime.datetime.now()))
+                    await message.channel.send("wake up and current time is: " + str(datetime.datetime.now()) + ('%s' % myid))
+                    alarmMinute = (alarmMinute + 1) % 24
 
 
-@client.command()
-async def orphan(ctx):
-    answers = [
-        "are you lost? should I call your parents?"
-        "i'll call your mum and dad, don't worry,"
-        "see  that kid over there? ask him about his parents,"
-        "do you want some candy?"
-    ]
-client.run('ODczMTYzNzU5MDg1MDI3MzM4.YQ0bHg.VTIWZdNSdquNnKTglf-2bRv0Cxc')
+'''intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)'''
 
+
+'''myvar = 'myself'
+newline = '\n'  # Avoids SyntaxError: f-string expr cannot include a backslash
+
+with open('token.txt', 'r') as file:
+    myfile = f"{file.read().replace(newline, '')}".format(**locals())
+'''
+# client.run('ODczMTYzNzU5MDg1MDI3MzM4.YQ0bHg._YC3giVlbiR9d7tfHA1LX1VAnh8')
+'''def read_token():
+    with open("token.txt"), "r") as file:
+    myfile = file.readlines()
+    return myfile[0].strip()'''
 '''def job():
     print("I'm working...")
 
@@ -37,15 +84,10 @@ schedule.every().minute.at(":17").do(job)
 while True:
     schedule.run_pending()
     time.sleep(1)'''
-
-def task():
-    print("Water Parade and Move About Time")
-
-
 print('git works on pycharm')
 print(datetime.datetime.now())
 
-def job():
+'''def job():
     print(datetime.datetime.now())
     alarmHour = datetime.datetime.now().hour
     alarmMinute = datetime.datetime.now().minute
@@ -55,13 +97,11 @@ def job():
     while True:
         if stopHour == datetime.datetime.now().hour and stopMinute == datetime.datetime.now().minute:
             print('End'+ str(datetime.datetime.now()))
+            stoptime = ('End'+ str(datetime.datetime.now()))
             break
         elif alarmHour == datetime.datetime.now().hour and alarmMinute == datetime.datetime.now().minute:
             print("wake up and current time is: " + str(datetime.datetime.now()))
-            alarmMinute = alarmMinute + 1
+            alarmMinute = alarmMinute + 1'''
+client.run(token)
+#schedule.every(4).minutes.do(job)
 
-schedule.every(4).minutes.do(job)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
